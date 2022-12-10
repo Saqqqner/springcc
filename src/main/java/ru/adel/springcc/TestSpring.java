ffff33e14fde6aec63ca15f1250334aeef499185
Saqqqner <rikardofik@gmail.com>
2022-12-10 21:57:37 +0300

    Создал коммит через баш

--- src/main/java/ru/adel/springcc/TestSpring.java
+++ src/main/java/ru/adel/springcc/TestSpring.java
@@ -10,7 +10,7 @@ public class TestSpring {
         MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
-
+        System.out.println("sout");
 
 
     }
